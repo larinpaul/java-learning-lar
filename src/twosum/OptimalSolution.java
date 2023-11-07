242c2b017bb922e57340289fa80ced05397ca959
Pavel Larin <www.larinpavel@mail.ru>
2023-11-07 21:32:42 +0500

    Two Sum, Suboptimal and Optimal solutions

---
+++ src/twosum/OptimalSolution.java
@@ -0,0 +1,20 @@
+package twosum;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class OptimalSolution {
+
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> complements = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			Integer complementIndex = complements.get(nums[i]);
+			if (complementIndex != null) {
+				return new int[] {i, complementIndex};
+			}
+			complements.put(target - nums[i],  i);
+		}
+		return nums;
+	}
+	
+}
