49fe0d0836a539a739c3993aaf8270cb0feeea94
Pavel Larin <www.larinpavel@mail.ru>
2023-11-10 02:13:10 +0500

    PingPong (to re-write)

--- src/module-info.java
+++ src/module-info.java
@@ -6,4 +6,7 @@
  *
  */
 module javalearning {
+	
+	requires java.desktop;
+	
 }
---
+++ src/pingpong/PongGame.java
@@ -0,0 +1,11 @@
+package pingpong;
+
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Let's play! :)");
+		GameFrame frame = new GameFrame();
+		
+	}
+}
