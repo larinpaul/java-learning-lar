67d43c689001ed1f347dc7c2d25e67bb1934c1c0
Pavel Larin <www.larinpavel@mail.ru>
2023-11-12 01:43:48 +0500

    POJOs and JavaBeans

---
+++ src/whatispojoandbean/BeanCat.java
@@ -0,0 +1,30 @@
+package whatispojoandbean;
+
+import java.io.Serializable;
+
+// Serializable means that this can be written into databases and files
+public class BeanCat implements Serializable {
+
+	private int age;
+	private String name;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+		
+	// To be a JavaBean:
+	// 1. No-args constructor
+	// 2. Properties must be private
+	// 3. Public getters and setters
+	// 4. Must be serializable
+	
+}
---
+++ src/whatispojoandbean/Cat.java
@@ -0,0 +1,13 @@
+package whatispojoandbean;
+
+public class Cat {
+
+	int age;
+	String name;
+	
+	// A POJO (Plain Old Java Object):
+	// 1. Can't Extend anything
+	// 2. Can't Implement anything
+	// 3. No outside annotations
+	
+}
