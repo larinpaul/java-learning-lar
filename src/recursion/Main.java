e657ec4a62ecf8bcce48e749fab6e1ec67d628f8
larinpaul <www.larinpavel@mail.ru>
2023-11-19 00:24:56 +0500

    Recursion

---
+++ src/recursion/Main.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// What recursion is?
+		// All recursion is, is when you have a method that calls itself
+
+		
+		
+	}
+
+}
