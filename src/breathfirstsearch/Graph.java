69190fb651359f0de708a16b3ab7c13d0acc9853
Pavel Larin <www.larinpavel@mail.ru>
2023-11-21 15:34:47 +0500

    Breadth First Search implemented via Java Queue LinkedList

--- src/breathfirstsearch/Graph.java
+++ src/breathfirstsearch/Graph.java
@@ -1,51 +1,54 @@
-package breadthfirstsearch;
+package breathfirstsearch;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
-	
+
 	ArrayList<Node> nodes;
 	int[][] matrix;
 	
-	Graph(int size){
-		
+	Graph(int size) {
 		nodes = new ArrayList<>();
 		matrix = new int[size][size];
 	}
+	
 	public void addNode(Node node) {
-		
 		nodes.add(node);
-	}	
+	}
+	
 	public void addEdge(int src, int dst) {
-		
 		matrix[src][dst] = 1;
-	}	
+	}
+	
 	public boolean checkEdge(int src, int dst) {
-		
-		if(matrix[src][dst] == 1) {
+		if (matrix[src][dst] == 1) {
 			return true;
 		}
 		else {
 			return false;
 		}
-	}	
-	public void print() {	
+	}
+	
+	public void print() {
 		
-		System.out.print("  ");
-		for(Node node : nodes) {
+		System.out.print(" ");
+		for (Node node : nodes) {
 			System.out.print(node.data + " ");
 		}
 		System.out.println();
 		
-		for(int i = 0; i < matrix.length; i++) {
+		for (int i = 0; i < matrix.length; i++) {
 			System.out.print(nodes.get(i).data + " ");
-			for(int j = 0; j < matrix[i].length; j++) {
+			for (int j = 0; j < matrix[i].length; j++) {
 				System.out.print(matrix[i][j] + " ");
 			}
 			System.out.println();
 		}
 		System.out.println();
-	}	
+	}
+	
 	public void breadthFirstSearch(int src) {
 		
 		Queue<Integer> queue = new LinkedList<>();
@@ -54,17 +57,20 @@ public class Graph {
 		queue.offer(src);
 		visited[src] = true;
 		
-		while(queue.size() != 0) {
+		while (queue.size() != 0) {
 			
 			src = queue.poll();
 			System.out.println(nodes.get(src).data + " = visited");
 			
-			for(int i = 0; i < matrix[src].length; i++) {
+			for (int i = 0; i < matrix[src].length; i++) {
 				if(matrix[src][i] == 1 && !visited[i]) {
 					queue.offer(i);
 					visited[i] = true;
 				}
 			}
 		}
+		
+		
 	}
+	
 }
