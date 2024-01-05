078b06b7e118862da7d535a802447f188e52e3a6
Pavel Larin <www.larinpavel@mail.ru>
2024-01-05 13:21:26 +0500

    Examples of some design patterns used in the Spring Framework

--- src/springframeworkdesignpatterns/templatemethod/PatternsApplication.java
+++ src/springframeworkdesignpatterns/templatemethod/PatternsApplication.java
@@ -1,22 +1,22 @@
-package springframeworkdesignpatterns.templatemethod;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-
-public class PatternsApplication {
-
-	public static void main(String[] args) {
-		
-		JdbcTemplate; // This class a lot of boilerplate code that is needed to create a JDBC connection
-		// But it's a little bit different that a definition of a template method
-		// but, in the end, it's the same concept
-		// that we put the boilerplate in our template class
-
-		// There is a slight difference
-		// between this design pattern in books
-		// and in the Spring implementation of them.
-		// But, overall, it's basically the same
-		
-		
-	}
-
-}
+//package springframeworkdesignpatterns.templatemethod;
+//
+//import org.springframework.jdbc.core.JdbcTemplate;
+//
+//public class PatternsApplication {
+//
+//	public static void main(String[] args) {
+//		
+//		JdbcTemplate; // This class a lot of boilerplate code that is needed to create a JDBC connection
+//		// But it's a little bit different that a definition of a template method
+//		// but, in the end, it's the same concept
+//		// that we put the boilerplate in our template class
+//
+//		// There is a slight difference
+//		// between this design pattern in books
+//		// and in the Spring implementation of them.
+//		// But, overall, it's basically the same
+//		
+//		
+//	}
+//
+//}
