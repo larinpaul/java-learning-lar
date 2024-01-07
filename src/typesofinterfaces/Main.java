29bf8d5e7cad7fff0237f8c681e5ee0cd59e3c24
Pavel Larin <www.larinpavel@mail.ru>
2024-01-07 20:42:10 +0500

    Types of Interface in Java

---
+++ src/typesofinterfaces/Main.java
@@ -0,0 +1,15 @@
+package typesofinterfaces;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Types of Interface:
+		// 1) Normal
+		// 2) Functional / SAM (Single Abstract Method)
+		// 3) Marker (for serialization/deserialization purposes)
+		// 		Marker interfaces are blank
+		
+	}
+
+}
