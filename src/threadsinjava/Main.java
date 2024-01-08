099d87046af44b9f4966434c0730420e30fa6207
Pavel Larin <www.larinpavel@mail.ru>
2024-01-08 20:02:35 +0500

    Theory of threads

---
+++ src/threadsinjava/Main.java
@@ -0,0 +1,20 @@
+package threadsinjava;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//	    [ sw1 ]             [sw2]        [sw3]
+//	  [   os    ]
+//	[     hw      ] -> CPU
+//	[  	          ] -> RAM
+
+	// In the same unit of a software,
+	// you can have several threads running at the same time
+	// multiple threads can have resources,
+	// and so, for example,
+	// a java process in the os can have 40 threads
+		
+	}
+
+}
