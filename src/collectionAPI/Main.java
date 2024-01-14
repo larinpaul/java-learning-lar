02cc300b6ee69b2ed1cfde757461e8a24e8a0d82
Pavel Larin <www.larinpavel@mail.ru>
2024-01-14 23:23:31 +0500

    Bird's eye overview of the Collection API in Java, Collection interface, Collections class

---
+++ src/collectionAPI/Main.java
@@ -0,0 +1,25 @@
+package collectionAPI;
+
+import java.util.Collections;
+
+public class Main {
+	
+	// Collection API - An API
+	// Collection - an interface implemented by many collections
+	// Collections - a class with many useful static methods
+
+	int num = 5;
+	int nums[] = new int[5];
+	int nums2[] = new int[7];
+	
+	
+	// Stack [] <</>>
+	// Queue << [] <<
+			
+			
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
